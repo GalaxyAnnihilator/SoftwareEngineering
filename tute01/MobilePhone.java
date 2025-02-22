f047e1cd4670d22a075575215383b7a5d6e5a014
tmd <minhduongqo@gmail.com>
2025-02-22 09:13:46 +0700

    finished tutor 1, added both classes Person and MobilePhone

---
+++ tute01/MobilePhone.java
@@ -0,0 +1,242 @@
+package tute01;
+import utils.*;
+
+/*
+ * @overview: A communication device
+ * 
+ * @attributes
+ *  manName     String
+ *  model       String
+ *  color       Character
+ *  year        Integer     int
+ *  guaranteed  Boolean
+ * 
+ * @object
+ *  A typical phone is <n,m,c,y,g> where manName(n),model(m),color(c),year(y),guaranteed(g)
+ * 
+ * @abstract_properties
+ *  mutable(manName) = true /\ optional(manName) = false /\ length(manName) = 30 /\
+ *  mutable(model) = false /\ optional(model) = false /\ length(model) = 30 /\
+ *  mutable(color) = false /\ optional(color) = false /\ length(color) = 1 /\
+ *  mutable(year) = false /\ optional(year) = false /\ min(year) = 1 /\
+ *  mutable(guaranteed) = true /\ optional(guaranteed) = false 
+ * 
+ */
+public class MobilePhone {
+    
+    @DomainConstraint(type="String", mutable=true, optional=false, length=30)
+    private String manName;
+
+    @DomainConstraint(type="String", mutable=false, optional=false, length=30)
+    private String model;
+
+    @DomainConstraint(type="Character", mutable=false, optional=false, length=1)
+    private Character color;
+
+    @DomainConstraint(type="Integer", mutable=false, optional=false, min=1)
+    private int year;
+
+    @DomainConstraint(type="Boolean", mutable=true, optional=false)
+    private Boolean guaranteed;
+
+    /*
+     * @effects
+     *      if manName is valid
+     *          return true
+     *      else
+     *          return false 
+     */
+    @DOpt(type=OptType.Helper) @AttrRef("manName")
+    private boolean validateManName(String manName) {
+        return manName != null && manName.length() <= 30;
+    }
+
+    /*
+     * @effects
+     *      if model is valid
+     *          return true
+     *      else
+     *          return false 
+     */
+    @DOpt(type=OptType.Helper) @AttrRef("model")
+    private boolean validateModel(String model) {
+        return model != null && model.length() <= 30;
+    }
+
+    /*
+     * @effects
+     *      if color is valid
+     *          return true
+     *      else
+     *          return false 
+     */
+    @DOpt(type=OptType.Helper) @AttrRef("color")
+    private boolean validateColor(Character color) {
+        return color != null && "ROYBP".indexOf(color) != -1;
+    }
+
+    /*
+     * @effects
+     *      if year is valid
+     *          return true
+     *      else
+     *          return false 
+     */
+    @DOpt(type=OptType.Helper) @AttrRef("year")
+    private boolean validateYear(int year) {
+        return year >= 1;
+    }
+
+    /*
+     * @effects
+     *      if guaranteed is valid
+     *          return true
+     *      else
+     *          return false 
+     */
+    @DOpt(type=OptType.Helper) @AttrRef("guaranteed")
+    private boolean validateGuaranteed(Boolean guaranteed) {
+        return guaranteed != null;
+    }
+
+    /*
+     * @effects
+     *  if n, m, c, y, g are valid
+     *      initialize this as <n,m,c,y,g>
+     *  else:
+     *      throw NotPossibleException
+     */
+    public MobilePhone(@AttrRef("manName") String manName, 
+                       @AttrRef("model") String model, 
+                       @AttrRef("color") Character color, 
+                       @AttrRef("year") int year, 
+                       @AttrRef("guaranteed") boolean guaranteed) throws NotPossibleException {
+        if (!validateManName(manName)) {
+            throw new NotPossibleException("Invalid manufacturer name");
+        }
+        if (!validateModel(model)) {
+            throw new NotPossibleException("Invalid model");
+        }
+        if (!validateColor(color)) {
+            throw new NotPossibleException("Invalid color");
+        }
+        if (!validateYear(year)) {
+            throw new NotPossibleException("Invalid year");
+        }
+        if (!validateGuaranteed(guaranteed)) {
+            throw new NotPossibleException("Invalid guaranteed value");
+        }
+        this.manName = manName;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.guaranteed = guaranteed;
+    }
+
+    /*
+     * @effects
+     *      return manName
+     */
+    @DOpt(type=OptType.Observer) 
+    @AttrRef("manName")
+    public String getManName() {
+        return manName;
+    }
+
+    /*
+     * @effects
+     *      return model
+     */
+    @DOpt(type=OptType.Observer) 
+    @AttrRef("model")
+    public String getModel() {
+        return model;
+    }
+
+    /*
+     * @effects
+     *      return color
+     */
+    @DOpt(type=OptType.Observer) 
+    @AttrRef("color")
+    public Character getColor() {
+        return color;
+    }
+
+    /*
+     * @effects
+     *      return year
+     */
+    @DOpt(type=OptType.Observer) 
+    @AttrRef("year")
+    public int getYear() {
+        return year;
+    }
+
+    /*
+     * @effects
+     *      return guaranteed
+     */
+    @DOpt(type=OptType.Observer) 
+    @AttrRef("guaranteed")
+    public Boolean getGuaranteed() {
+        return guaranteed;
+    }
+
+    /*
+     * @effects
+     *  if manName is valid
+     *      set this.manName = manName
+     *      return true
+     *  else
+     *      return false
+     */
+    @DOpt(type=OptType.Mutator)
+    @AttrRef("manName")
+    public boolean setManName(String manName) {
+        if (validateManName(manName)) {
+            this.manName = manName;
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * @effects
+     *  if guaranteed is valid
+     *      set this.guaranteed = guaranteed
+     *      return true
+     *  else
+     *      return false
+     */
+    @DOpt(type=OptType.Mutator)
+    @AttrRef("guaranteed")
+    public boolean setGuaranteed(Boolean guaranteed) {
+        if (validateGuaranteed(guaranteed)) {
+            this.guaranteed = guaranteed;
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * @effects
+     *      return true if the object satisfies its constraints
+     */
+    public boolean repOK() {
+        return validateManName(manName) &&
+               validateModel(model) &&
+               validateColor(color) &&
+               validateYear(year) &&
+               validateGuaranteed(guaranteed);
+    }
+
+    /*
+     * @effects
+     *      return string representation of the object
+     */
+    @Override
+    public String toString() {
+        return String.format("MobilePhone: <%s, %s, %c, %d, %b>", manName, model, color, year, guaranteed);
+    }
+}
---
+++ tute01/Person.java
@@ -0,0 +1,182 @@
+package tute01;
+import utils.*;
+
+/*
+ * @overview: A human being
+ * 
+ * @attributes
+ *  id      Integer     int
+ *  name    String
+ *  phone   MobilePhone
+ * 
+ * @object
+ *  A typical person is <i,n,p> where id(i), name(n), phone(p)
+ * 
+ * @abstract_properties
+ *  mutable(id) = false /\ optional(id) = false /\ min(id) = 1 /\
+ *  mutable(name) = true /\ optional(name) = false /\ length(name) = 30 /\
+ *  mutable(phone) = true /\ optional(phone) = true /\
+ */
+
+public class Person {
+    @DomainConstraint(type="Integer",mutable=false,optional=false,min=1)
+    private int id;
+
+    @DomainConstraint(type="String",mutable=true,optional=false,length = 30)
+    private String name;
+
+    @DomainConstraint(type="MobilePhone",mutable=true,optional = true)
+    private MobilePhone phone;
+
+    /*
+     * @effects
+     *      if id is valid
+     *          return true
+     *      else
+     *          return false
+     */
+    private boolean validateID(int id){
+        return id >= 1;
+    }
+
+    /*
+     * @effects
+     *      if name is valid
+     *          return true
+     *      else
+     *          return false
+     * 
+     */
+    private boolean validateName(String name){
+        return name.length() <= 30;
+    }
+
+    /*
+     * @effects
+     *      if phone is valid
+     *          return true
+     *      else
+     *          return false
+     */
+
+    private boolean validatePhone(Object phone){
+        return phone instanceof MobilePhone;
+    }
+
+    /*
+     * @effects
+     *  if i,n,p are valid:
+     *      initialize this as <tt> <i,n,p> </tt>
+     *  else:
+     *      throw NotPOssibleException
+     */
+    public Person(@AttrRef("id") int id, @AttrRef("name") String name, @AttrRef("phone") MobilePhone phone) throws NotPossibleException{
+        if (!validateID(id)){
+            throw new NotPossibleException("Invalid id!");
+        }
+        if (!validateName(name)){
+            throw new NotPossibleException("Invalid name!");
+        }
+        if (!validatePhone(phone)){
+            throw new NotPossibleException("Invalid phone!");
+        }
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    /*
+     * @effects
+     *      return id 
+     */
+    @DOpt(type=OptType.Observer) @AttrRef("id")
+    public int getId(){
+        return this.id;
+    }
+
+    /*
+     * @effects
+     *      return name 
+     */
+    @DOpt(type=OptType.Observer) @AttrRef("name")
+    public String getName(){
+        return this.name;
+    }
+    
+    /*
+     * @effects
+     *      return phone 
+     */
+    @DOpt(type=OptType.Observer) @AttrRef("phone")
+    public MobilePhone getPhone(){
+        return this.phone;
+    }
+
+    /*
+     * @effects
+     *  if id is valid
+     *      set this.id = id
+     *      return true
+     *  else
+     *      return false
+     * 
+     */
+    @DOpt(type=OptType.Mutator) @AttrRef("id")
+    public boolean setID(int id){
+        if (validateID(id)){
+            this.id = id;
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * @effects
+     *  if name is valid
+     *      set this.name = name
+     *      return true
+     *  else
+     *      return false
+     * 
+     */
+    @DOpt(type=OptType.Mutator) @AttrRef("name")
+    public boolean setName(String name){
+        if (validateName(name)){
+            this.name = name;
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * @effects
+     *  if phone is valid
+     *      set this.phone = phone
+     *      return true
+     *  else
+     *      return false
+     * 
+     */
+    @DOpt(type=OptType.Mutator) @AttrRef("phone")
+    public boolean setPhone(MobilePhone phone){
+        if (validatePhone(phone)){
+            this.phone = phone;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Person: <%d,%s,%s>",this.id,this.name,this.phone);
+    }
+
+    /*
+     * @effects
+     *  return true if id,name,phone are valid 
+     * 
+     */
+    public boolean repOK(){
+        return (validateID(id)&&validateName(name)&&validatePhone(phone));
+    }
+}
